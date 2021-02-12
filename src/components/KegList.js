ef3b560286ab93483a131658a8b96c47a6f5f417
Zahnen Garner <zahnen@gmail.com>
2021-02-12 09:04:44 -0800

    add keglist component

---
+++ src/components/KegList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Keg from "./Keg";
+import PropTypes from "prop-types";
+
+function KegList(props) {
+  return(
+    <>
+      {props.kegList.map((keg) =>
+        <Keg
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          abv={keg.abv}
+          id={keg.id}
+          key={keg.id}
+          pintsLeft={keg.pintsLeft} />
+      )}
+    </>
+  );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+};
+
+export default KegList;
