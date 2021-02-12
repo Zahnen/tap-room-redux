82f4d0d5b8455a71afa257220f9918151bae99fd
Zahnen Garner <zahnen@gmail.com>
2021-02-12 08:53:00 -0800

    add keg component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return(
+    <>
+      <div class="container">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Keg.js
@@ -0,0 +1,25 @@
+import React from 'React';
+import PropTypes from "prop-types";
+
+function Keg(props){
+  return (
+    <>
+      <h1>props.name</h1>
+      <h1>props.brand</h1>
+      <h1>props.price</h1>
+      <h1>props.abv</h1>
+      <h1>props.pintsLeft</h1>
+    </>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  abv: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  pintsLeft: PropTypes.number.isRequired
+};
+
+export default Keg;
