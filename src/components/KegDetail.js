04e5e2a8cf04da787b1ba511301d1386524fca29
Zahnen Garner <zahnen@gmail.com>
2021-02-12 13:55:50 -0800

    add dynamic display for low inventory alert

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -9,7 +9,7 @@ function KegDetail(props){
       <h3>Brewer: {keg.brand}</h3>
       <h3>ABV: {keg.abv}</h3>
       <h3>Price Per Pint: {keg.price}</h3>
-      {keg.pintsLeft === 0 ? <h3>Out of Stock</h3> : <h3>Pints Remaining in Keg: {keg.pintsLeft}</h3>}
+      {keg.pintsLeft === 0 ? <h3>Out of Stock</h3> : keg.pintsLeft < 10 ? <h3>Low Stock! Refill Soon! Pints Remaining: {keg.pintsLeft}</h3> : <h3>Pints Remaining in Keg: {keg.pintsLeft}</h3>}
       {keg.pintsLeft > 0 ? <button onClick={props.onClickingSell}>Sell a Pint</button> : null}
     </>
   );
