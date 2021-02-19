932c21bd47cdc07af13f9f83d51e4676df26c683
Zahnen Garner <zahnen@gmail.com>
2021-02-19 10:31:37 -0800

    write + fail second test for editing-reducer

--- src/__tests__/reducers/editing-reducer.test.js
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -5,4 +5,8 @@ describe('editingReducer', () => {
   test('Should return default state if no action is passed into reducer', () => {
     expect(editingReducer(false, { type: null})).toEqual(false);
   });
+
+  test('Should toggle form visible boolean from false to true', () => {
+    expect(editingReducer(false, {type: 'TOGGLE_EDIT'})).toEqual(true);
+  });
 });
