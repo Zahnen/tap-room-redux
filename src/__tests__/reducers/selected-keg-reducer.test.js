8e46b60593acd59b84d951394d03cb5b6eb79071
Zahnen Garner <zahnen@gmail.com>
2021-02-19 14:49:16 -0800

    rewrite/pass selected-keg-reducer tests to account for reducer change

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -2,42 +2,23 @@ import selectedKegReducer from '../../reducers/selected-keg-reducer';
 
 describe ('selectedKegReducer', () => {
 
-  let action;
-  const currentState = {
-    1 : {
-      name: 'Beer',
-      brand: 'Brewer',
-      price: '$2.50',
-      abv: '4.2',
-      id: 1,
-      pintsLeft: 124
-    },
-    2 : {
+  test('Should return default state if no action is passed into reducer', () => {
+    expect(selectedKegReducer(null, { type: null })).toEqual(null);
+  });
+
+  test('Should return a selected keg object', () => {
+    let selectedTestKeg = {
       name: 'Mead',
       brand: 'Brewer2',
       price: '$3.50',
       abv: '3.8',
       id: 2,
       pintsLeft: 9
-    }
-  }
-
-  test('Should return default state if no action is passed into reducer', () => {
-    expect(selectedKegReducer(null, { type: null })).toEqual(null);
-  });
-
-  test('Should return a selected keg object', () => {
-    action = {
+      }
+    let action = {
       type: 'SELECT_KEG',
-      id: 2
-    };
-    expect(selectedKegReducer(currentState, action)).toEqual({
-        name: 'Mead',
-        brand: 'Brewer2',
-        price: '$3.50',
-        abv: '3.8',
-        id: 2,
-        pintsLeft: 9
-      });
+      selectedKeg: selectedTestKeg
+    }
+    expect(selectedKegReducer(null, action)).toEqual(selectedTestKeg);
   });
 });
