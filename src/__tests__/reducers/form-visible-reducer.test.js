09c470b9c449ce2cc7f02550e57ae1c20c3b12f3
Zahnen Garner <zahnen@gmail.com>
2021-02-19 10:12:36 -0800

    write + fail second test for form-visible-reducer

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -4,4 +4,8 @@ describe('formVisibleReducer', () => {
   test('Should return default state if no action is passed into reducer', () => {
     expect(formVisibleReducer(false, { type: null})).toEqual(false);
   });
+
+  test('Should toggle form visible boolean from false to true', () => {
+    expect(formVisibleReducer(false, {type: 'TOGGLE_FORM'})).toEqual(true);
+  });
 });
