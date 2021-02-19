1ca5499c336516030696e0dd5b6a3ba62dae29a4
Zahnen Garner <zahnen@gmail.com>
2021-02-19 13:21:32 -0800

    pass sanity check for form-visible-reducer and root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -21,4 +21,8 @@ describe ('rootReducer', () => {
   test('Check that initial state of masterKegListReducer matches root reducer', () => {
     expect(store.getState().masterKegList).toEqual(masterKegListReducer(undefined, { type: null}));
   });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null}));
+  });
 });
