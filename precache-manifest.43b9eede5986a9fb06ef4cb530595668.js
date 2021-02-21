a28528c0ba47674e835797fff0eb3f61a33f3140
Zahnen Garner <zahnen@gmail.com>
2021-02-21 11:42:29 -0800

    Updates

---
+++ precache-manifest.43b9eede5986a9fb06ef4cb530595668.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76b11def5996803ec8304ecbe679cfa",
+    "url": "/tap-room-redux/index.html"
+  },
+  {
+    "revision": "d6cbb5bd464ea07acbf4",
+    "url": "/tap-room-redux/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "3340d752482c6a6bff6a",
+    "url": "/tap-room-redux/static/css/main.e2ef1e74.chunk.css"
+  },
+  {
+    "revision": "d6cbb5bd464ea07acbf4",
+    "url": "/tap-room-redux/static/js/2.8d345c01.chunk.js"
+  },
+  {
+    "revision": "1da2ab2eab5169a4d495",
+    "url": "/tap-room-redux/static/js/3.41929e81.chunk.js"
+  },
+  {
+    "revision": "3340d752482c6a6bff6a",
+    "url": "/tap-room-redux/static/js/main.0b1b502c.chunk.js"
+  },
+  {
+    "revision": "040f015394274e74798a",
+    "url": "/tap-room-redux/static/js/runtime-main.d18bdc17.js"
+  },
+  {
+    "revision": "4b4fe5abf33de002cfce472787ec3535",
+    "url": "/tap-room-redux/static/media/HopperLogo.4b4fe5ab.svg"
+  },
+  {
+    "revision": "832633201b5c7d83ec7e2e1e0ff0b8dd",
+    "url": "/tap-room-redux/static/media/KegEmpty.83263320.svg"
+  },
+  {
+    "revision": "31027d5e3fe307ce2c03f19584eefe15",
+    "url": "/tap-room-redux/static/media/KegFull.31027d5e.svg"
+  },
+  {
+    "revision": "dd6e433a12afba336f70f8430376c539",
+    "url": "/tap-room-redux/static/media/KegLow.dd6e433a.svg"
+  }
+]);
